7f487eae3a2e0b6365fb1bc270347c163072c0e3
Moonseon <moonseonyoon@gmail.com>
2024-02-20 23:01:54 +0900

    added imgs and changed text

---
+++ data.js
@@ -0,0 +1,45 @@
+const data = [
+  {
+    itemName: "FancyClub",
+    itemCategory: "Web Design",
+    itemLink: "https://www.figma.com/file/5DilBf9fnE2KcYOPaPhyTq/FancyClub-Redesign?type=design&node-id=0%3A1&mode=design&t=0Iwor4NJWgFzHuib-1",
+    itemCopy:
+      "Redesigned a clothing shop I enjoy!",
+    itemImg: "./img/fancyclub preview 1.png",
+  },
+  {
+    itemName: "Catzone",
+    itemCategory: "Mobile App",
+    itemLink:
+      "https://www.figma.com/file/jEcjgsJ82bXkBMkF1Pcjd1/Catzone?type=design&mode=design&t=jq6SIbWZmRvywnt1-1",
+    itemCopy:
+      "The inspiration for this app was when I was on a trip to Yang Yang, South Korea. Within 2 days of my trip, I saw a total of 10~13 stray cats around the area. Upon further research, I've learned populations run rampant within the cities in Korea. These poor strays have a life expectancy of only 5 years, most of which die due to car accidents. My purpose for Catzone is to encourage others to take care of these strays humanely. NOTE: Illustrations and drawings used in this design were not made by me. This project was made purely for practice and to showcase my skills.",
+    itemImg: "./img/cat preview 2.png",
+  },
+  {
+    itemName: "Portfolio Website",
+    itemCategory: "Freelance: Web Design, Web Development",
+    itemLink: "https://www.figma.com/file/cXlrajhrLFP2g6paJAXEne/Client---Alex?type=design&node-id=0%3A1&mode=design&t=1JuEIQSZyp8pxYOx-1",
+    itemCopy:
+      "Created a portfolio website for a student who majors in Architect Design. I am responsible for every aspect of the project, from designing the user experience and user interface to coding the front-end. The website will launch live soon!",
+    itemImg: "./img/alex portfolio preview.png",
+  },
+  {
+    itemName: "Music UI",
+    itemCategory: "UI Design",
+    itemLink: "https://www.figma.com/file/flPHhNWa3FbttLNHGGL8ho/Music-UI-Components?type=design&node-id=0%3A1&mode=design&t=Q3Yv2OedfvApTxUs-1",
+    itemCopy:
+      "Made some UI's for a music app. Features design concepts for different devices such as the apple watch, smartphone, and widgets!",
+    itemImg: "./img/music ui components.png",
+  },
+  {
+    itemName: "REVISION",
+    itemCategory: "Graphic Design",
+    itemLink: "https://moonseonyoon.notion.site/moonseonyoon/Moonseon-Yoon-Alice-27238e6a9cd9460e90427c7c489ed68c?p=ef25a73ef33d405a8ae8097984201f02&pm=c",
+    itemCopy:
+      "Design concept I made in my free time. T-shirt mockups are in the link!",
+    itemImg: "./img/revision main.png",
+  }
+];
+
+export { data };
